fe9be1d83b0cf1d803aeb2e043b04bdf45051b9a
richwork108 <richwork1@gmail.com>
2024-12-06 09:45:25 +0100

    Few more updates

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { get_online_users, login, reset_password } from "../controllers/auth.controller.js";
+import { get_active_staff, get_online_users, login, reset_password } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.get("/get_online_users", get_online_users)
 router.post('/login', login)
 router.post('/reset_password/:id', reset_password)
+router.post('/get_active_staff/:id', get_active_staff)
 
 export default router;
